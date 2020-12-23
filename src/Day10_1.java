fe6dace07d59c7fae0d9b3e4b86f0752ea6bbad8
DavidFM43 <dmorara@unal.edu.co>
2020-12-22 22:29:37 -0500

    Day10

--- src/Day10_1.java
+++ src/Day10_1.java
@@ -1,4 +1,7 @@
 import java.util.*;
+
+
+
 import java.io.File;
 
 public class Day10_1 {
@@ -6,6 +9,37 @@ public class Day10_1 {
 
         File file = new File("C:\\Users\\user\\Desktop\\Test\\AdventOfCode\\src\\input");
         Scanner scan = new Scanner(file);
+        
+        int size= 92;
+        TreeSet<Integer> adapters = new TreeSet<>();
+        int joltage = 0;
+        int onedif = 0;
+        int threedif = 0;
+
+        while(scan.hasNext()){
+            int num = Integer.parseInt(scan.nextLine());
+            adapters.add(num);    
+        }
+
+        int highest = adapters.last()+3;
+        while(!adapters.isEmpty()){
+            int adapter = adapters.first();
+            int dif = adapter - joltage;
+            if (dif == 1)
+                onedif++;
+            else
+                threedif++;
+            joltage = adapter;
+            adapters.remove(adapter);
+        }
+        if(highest-joltage==1)
+            onedif++;
+        else
+            threedif++;
+
+
+        System.out.println(onedif*threedif);
+
     
     }
 }
