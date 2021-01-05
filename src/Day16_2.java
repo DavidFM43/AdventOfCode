4fec8b8305cb33601029f8e470935e4670e440e1
David <dmorara@unal.edu.co>
2021-01-04 23:45:13 -0500

    Day 16

--- src/Day16_2.java
+++ src/Day16_2.java
@@ -2,14 +2,14 @@ import java.util.*;
 import java.io.File;
 public class Day16_2{
     public static void main(String[] args)throws Exception {
-        File file = new File("C:\\Users\\user\\Desktop\\Test\\AdventOfCode\\src\\input");
+        File file = new File("//home//david//eclipse-workspace//AdventOfCode1//src//input");
         Scanner scan = new Scanner(file);
         int cont = 0; 
         int ans = 0;
-        int[][][] ranges = new int[20][2][2];
-        HashSet<Integer> invalid = new HashSet<Integer>();
-        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>(); 
-        for(int i = 0; i < 20; i++ ){
+        HashMap<Integer, HashSet<Integer> > map = new HashMap<>();
+        int size = 20;
+        int[][][] ranges = new int[size][2][2];
+        for(int i = 0; i < size; i++ ){
             String line = scan.nextLine();
             int[] first = toIntArr(line.split(":")[1].split("or")[0].trim().split("-"));
             int[] second = toIntArr(line.split(":")[1].split("or")[1].trim().split("-"));
@@ -17,11 +17,13 @@ public class Day16_2{
             ranges[cont][1] = second;
             cont++;
         }           
-
-        for(int i = 0; i < 5; i++){
+        for(int i = 0; i < 2; i++){
             scan.nextLine();
         }
-
+        String myTicket = scan.nextLine();
+        scan.nextLine();
+        scan.nextLine();
+        
         String stringTickets = "";
 
         while(scan.hasNextLine()){
@@ -47,27 +49,82 @@ public class Day16_2{
         stringTickets += stringTicket+" ";
 }
     String[] tickets = stringTickets.split(" ");
-    for(int j = 0; j < 20; j++){
+        //System.out.println(tickets.length);
+    for(int i = 0; i < size; i++){
+        int cont2 = 0;
+    for(int j = 0; j < size; j++){
         int contador = 0;
     for(String cosa: tickets){                        
         int value = Integer.parseInt(cosa.split(",")[j]);
-        if(value < ranges[0][0][0] || value > ranges[0][0][1]) {
-            System.out.println(value);
-        }else if (value < ranges[0][1][0] || value > ranges[0][1][1]){
-            System.out.println(value);
+           // System.out.println(value+" first["+ranges[0][0][0]+", "+ranges[0][0][1]+"]"+" second["+ranges[0][1][0]+", "+ranges[0][1][1]+"]");
+        if(value < ranges[i][0][0] || value > ranges[i][0][1]) {
+            if(value < ranges[i][1][0] || value > ranges[i][1][1]){
+        }else{
+            contador++;
+        }
         }else{
             contador++;
         }
     }
-    System.out.println("Campo: "+j+". Check:"+contador);
+    if(contador == tickets.length)
+        if(map.get(j) == null){
+            HashSet<Integer> values = new HashSet<>();
+            values.add(i); 
+            map.put(j,values);
+        }else{
+            HashSet<Integer> values = map.get(j);
+            values.add(i);
+            map.put(j,values);
+
+        }
+        cont2++;
+    }
+            //System.out.println(cont2); 
+            //System.out.println("first["+ranges[i][0][0]+", "+ranges[i][0][1]+"]"+" second["+ranges[i][1][0]+", "+ranges[i][1][1]+"]");
+    }
+    //la llave j califica para los campos que tiene en el HashMap. 
+    HashMap<Integer,Integer> answer = new HashMap<Integer,Integer>();
+    while(true){
+        boolean test = true;
+        for(int i = 0; i < size; i++){
+            if(map.get(i).size() == 1){
+                Iterator it = map.get(i).iterator();
+                int num = (int) it.next();
+                answer.put(num,i);
+                map = update(map,num);
+                test = false;
+                break;
+            }
+        }
+        if(test == true)
+            break;
+    }
+   long res = 1;
+        System.out.println(myTicket);
+    for(int i = 0; i < 6; i++){
+        res *= Integer.parseInt(myTicket.split(",")[answer.get(i)]);
+            System.out.println("Key "+i+" value "+answer.get(i));
+            
+    }
+    System.out.println(res);
     }
 /*
 for(Integer key:map.keySet()){
     System.out.println("Key: "+key+". Value: "+map.get(key));
 }
 */
-
-
+public static void print(HashMap<Integer, HashSet<Integer>> map){
+     for(Integer key : map.keySet()){
+            System.out.println("Key "+key+" Values: "+map.get(key));
+    }
+}
+public static HashMap<Integer, HashSet<Integer>> update(HashMap<Integer, HashSet<Integer>> map, int value){
+    for(Integer key : map.keySet()){
+        HashSet<Integer> set = map.get(key);
+        set.remove(value);
+        map.put(key,set);
+    }
+    return map;
 }
 public static int[] toIntArr(String[] stringArr){
     int[] intArray = new int[stringArr.length];
