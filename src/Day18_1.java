a289afc5362dbbb47d824d63b0ec744c861c4f8a
David <dmorara@unal.edu.co>
2021-01-16 21:51:42 -0500

    Day 16

---
+++ src/Day18_1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+import java.io.File;
+
+public class Day18_1 {
+    public static void main(String[] args) throws Exception {
+        File file = new File("//home//david//eclipse-workspace//AdventOfCode1//src//input");
+        Scanner scan = new Scanner(file);
+    }
+}
