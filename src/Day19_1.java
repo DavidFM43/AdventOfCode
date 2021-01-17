f60ecfa82965cd176e6101ce86e00032fa8cbc30
DavidFM43 <dmorara@unal.edu.co>
2021-01-16 21:27:49 -0500

    Day 19

---
+++ src/Day19_1.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.io.File;
+public class Day19_1 {
+    public static void main(String[] args) throws Exception{
+        File file = new File();
+        Scanner scan = new Scanner(file);
+    }
+}
